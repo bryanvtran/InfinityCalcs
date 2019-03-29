448c4689975a602ec629ab80112ad0c5af03b70e
Bryan Tran <bryanvantran@ucla.edu>
2019-03-28 21:33:32 -0700

    add images in markdown

--- src/components/resource-post.js
+++ src/components/resource-post.js
@@ -13,9 +13,12 @@ export default ({ data }) => {
         <CalcLayout title="Resources" extraStyles={[styles.resourcesHeader]}>
              <div css={[styles.container, styles.calcContainer, styles.resourceContainer]}>
                 <h2>{post.frontmatter.title}</h2>
-                <div css={styles.resourceVideo}>
-                    <iframe width="560" height="315" src={`https://www.youtube.com/embed/${post.frontmatter.video}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
+                {post.frontmatter.video && (
+                    <div css={styles.resourceVideo}>
+                        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${post.frontmatter.video}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                )}
+
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
             </div>
         </CalcLayout>
