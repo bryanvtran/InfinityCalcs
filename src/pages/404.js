6ac142cfa7bc5c6803f9eadbeaf64e27310d4a87
Bryan Tran <bryanvantran@ucla.edu>
2019-04-23 13:35:29 -0700

    404 fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+const browser = typeof window !== "undefined" && window;
+
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
